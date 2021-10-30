c4df4a4eea42d5a254c4928ae3835461a3e3ce6d
Shaun Pour <scpour@gmail.com>
2021-10-30 10:59:31 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
 function setClock() {
     let currentDate = new Date();
+    let day = currentDate.getDay();
+    let currentDay = currentDate.toLocaleString();
     let seconds = currentDate.getSeconds();
     let minute = currentDate.getMinutes();
     let hour = currentDate.getHours();
+
+    switch (day) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+    }
 
     let amPm = (hour < 12) ? "AM" : "PM";
 
